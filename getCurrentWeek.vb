cf9baafc88043915c1a1a8d3a9fa541c7bc4c0b1
Matt <maatspencer@gmail.com>
2016-09-12 17:18:54 -0400

    updated URL

--- getCurrentWeek.vb
+++ getCurrentWeek.vb
@@ -7,5 +7,8 @@
 ' To change this template use Tools | Options | Coding | Edit Standard Headers.
 '
 Public Class getCurrentWeek
-	
+	Public Shared Sub scrapeHTML
+		Dim sourceString As String = New System.Net.WebClient().DownloadString("http://www.fantasysharks.com/apps/Projections/WeeklyProjections.php?pos=ALL")
+		
+	End Sub
 End Class
